62b74ea19ab6bbd8e98176b40519959813f8cdde
d.florentina.calapod <>
2023-01-09 20:41:58 +0100

    end-task

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,69 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int keyNoteNumber) {
+    final player = AudioCache();
+    player.play('note$keyNoteNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text('Click me'),
-            ),
+          child: Column(
+            children: <Widget>[
+              TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.red),
+                onPressed: () {
+                  playSound(1);
+                },
+                child: Text(''),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.orange),
+                onPressed: () {
+                  playSound(2);
+                },
+                child: Text(''),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.yellow),
+                onPressed: () {
+                  playSound(3);
+                },
+                child: Text(''),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.green),
+                onPressed: () {
+                  playSound(4);
+                },
+                child: Text(''),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.green[900]),
+                onPressed: () {
+                  playSound(5);
+                },
+                child: Text(''),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.blue),
+                onPressed: () {
+                  playSound(6);
+                },
+                child: Text(''),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.purple),
+                onPressed: () {
+                  playSound(7);
+                },
+                child: Text(''),
+              ),
+            ],
           ),
         ),
       ),
